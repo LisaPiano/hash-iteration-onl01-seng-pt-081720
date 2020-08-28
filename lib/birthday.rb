597a1e9546ec028d010aeb0b07544cd244cfc8c0
Lisa Walters <campi_l@fortlewis.edu>
2020-08-28 19:19:17 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -8,9 +8,9 @@ def happy_birthday(birthday_kids)
     birthday_kids.each do |kids_name, age|
 
       puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
-if 
+if
   age >= 12
-  
+
   puts "You are too old for this."
 end
 
